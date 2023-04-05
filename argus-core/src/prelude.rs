6340a18149b9be8f1c197849a4f90b18bf902ba3
Anand Balakrishnan <anandbala1597@gmail.com>
2023-04-05 13:29:10 -0700

    feat(core): add NumExpr and BoolExpr to prelude

--- argus-core/src/prelude.rs
+++ argus-core/src/prelude.rs
@@ -1,3 +1,3 @@
-pub use crate::expr::{Expr, ExprBuilder, ExprRef};
+pub use crate::expr::{BoolExpr, Expr, ExprBuilder, ExprRef, NumExpr};
 pub use crate::signals::{AnySignal, ConstantSignal, Signal};
 pub use crate::{ArgusError, ArgusResult};
