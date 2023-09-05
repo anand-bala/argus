009a0ac18aa790b0056ca61a01236615675c8a65
Anand Balakrishnan <anandbala1597@gmail.com>
2023-09-05 10:28:12 -0700

    start argus-parser

---
+++ argus-parser/src/lib.rs
@@ -0,0 +1,16 @@
+//! # Argus logic syntax
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
