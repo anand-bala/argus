e2cff9449e20591cfdf41ad31cfe22e9fc33fceb
Anand Balakrishnan <anandbala1597@gmail.com>
2023-10-04 14:37:51 -0700

    feat: expose parser in argus

--- argus/src/lib.rs
+++ argus/src/lib.rs
@@ -1,3 +1,4 @@
 pub use argus_core::signals::{AnySignal, Signal};
 pub use argus_core::{expr, signals, ArgusResult, Error};
+pub use argus_parser::parse_str;
 pub use argus_semantics::{BooleanSemantics, QuantitativeSemantics, Trace};
