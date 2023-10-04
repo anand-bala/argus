db3a63f9e12720070e7dad38f9102294ee807e40
Anand Balakrishnan <anandbala1597@gmail.com>
2023-10-03 17:13:20 -0700

    make argus the common crate

--- argus/src/lib.rs
+++ argus/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub mod prelude {
+    pub use argus_core::prelude::*;
+    pub use argus_parser::parse_str;
+    pub use argus_semantics::{BooleanSemantics, QuantitativeSemantics, Trace};
 }
